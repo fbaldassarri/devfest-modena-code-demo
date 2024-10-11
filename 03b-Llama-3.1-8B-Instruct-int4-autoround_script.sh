287eb96f5f6b0684ca932923670480a11426027c
Francesco Baldassarri <f.baldassarri@live.com>
2024-10-11 20:17:57 +0200

    Update 03b-Llama-3.1-8B-Instruct-int4-autoround_script.sh

--- 03b-Llama-3.1-8B-Instruct-int4-autoround_script.sh
+++ 03b-Llama-3.1-8B-Instruct-int4-autoround_script.sh
@@ -1,6 +1,3 @@
-git clone https://github.com/intel/auto-round
-cd auto-round/examples/language-modeling
-pip install -r requirements.txt
 python3 main.py \
 --model_name  meta-llama/Meta-Llama-3.1-8B-Instruct \
 --device 0 \
