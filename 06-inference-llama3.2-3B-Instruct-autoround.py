c52ae31dbbcdd1eaaa13d791f0fbbba8eb1e9944
Francesco Baldassarri <f.baldassarri@live.com>
2024-10-11 20:16:49 +0200

    update

--- 06-inference-llama3.2-3B-Instruct-autoround.py
+++ 06-inference-llama3.2-3B-Instruct-autoround.py
@@ -1,14 +1,17 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from auto_round import AutoRoundConfig
+from auto_round.auto_quantizer import AutoHfQuantizer
 
-device = "cpu"  ##cpu, hpu, cuda
-quantization_config = AutoRoundConfig(
-    backend=device
-)
-quantized_model_path = "./AutoRound/Llama-3.2-3B-Instruct-AutoRound-GPTQ-sym"
-model = AutoModelForCausalLM.from_pretrained(quantized_model_path,
-                                             device_map=device, quantization_config=quantization_config)
-tokenizer = AutoTokenizer.from_pretrained(quantized_model_path)
+# quantized_model_dir = "Intel/Meta-Llama-3.1-8B-Instruct-AutoRound"
+quantized_model_dir = "./AutoRound/Llama-3.1-8B-Instruct-AutoRound-GPTQ-INT4-asym"
+tokenizer = AutoTokenizer.from_pretrained(quantized_model_dir)
+model = AutoModelForCausalLM.from_pretrained(quantized_model_dir,
+                                             device_map="auto"
+                                             )
+
+tokenizer = AutoTokenizer.from_pretrained(quantized_model_dir)
 text = "There is a girl who likes adventure,"
 inputs = tokenizer(text, return_tensors="pt").to(model.device)
 print(tokenizer.decode(model.generate(**inputs, max_new_tokens=50)[0]))
+
+text = "There is a girl who likes adventure,"
+
